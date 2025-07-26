d8261be21569bb1c9a6292d31be737109a35a76c
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-26 11:51:09 +0200

    removed default collection creation - collections now created only after fetching data, matching the resource

--- db/init-mongo.js
+++ db/init-mongo.js
@@ -1,9 +1,7 @@
 // Create first database (db1) with a collection
-db = db.getSiblingDB('users-demo')
-db.createCollection('users_data')
+db = db.getSiblingDB('users-data')
 
 // Create second database (db2) with a collection
-db = db.getSiblingDB('time-series-demo')
-db.createCollection('time_series_data')
+db = db.getSiblingDB('time-series-data')
 
 print("Initialized databases: users_data and times_series_data")
