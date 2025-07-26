b83732c50e9c128be02f0bdb81e61392fec648a0
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-26 13:05:48 +0200

    removed credentials and reference .env file

--- task-1/fetch-data.py
+++ task-1/fetch-data.py
@@ -2,8 +2,22 @@
 import aiohttp
 import asyncio
 import json
+import pymongo
+from dotenv import load_dotenv
+import os
 
-base_url = 'https://jsonplaceholder.typicode.com'
+load_dotenv()  # Load environment variables from .env file
+
+#fetch environment variables
+database_url = os.getenv("MONGO_URI")
+database_username = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+database_password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+base_url = os.getenv("JSON_PLACEHOLDER")
+
+# MongoDB connection setup
+mongo_client = pymongo.MongoClient(database_url, database_username, database_password)
+db = mongo_client['users-demo']  # Change to your database name
+
 resource_endpoints = ['posts', 'comments', 'users']
 
 async def fetch_data(url):
@@ -26,8 +40,16 @@ async def main():
         if data:
             # Log the fetched data
             print(f"Data from {endpoint}:")
-            # save the data to mongodb
-            print(json.dumps(data, indent=2))
-            print("\n")
+            try:
+            # save the data to mongodb collection matching the endpoint
+                collection = db[endpoint]
+                if isinstance(data, list):
+                    collection.insert_many(data)  # Insert multiple documents
+                else:
+                    collection.insert_one(data)  # Insert a single document
+                print(f"Data saved to MongoDB collection: {endpoint}")
+            except Exception as e:
+                print(f"Error saving data to MongoDB for {endpoint}")
+                print(f"Error: {e}")
 
 asyncio.run(main())
