0dc36fca9f77cf044919512236499a99fac39f80
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-27 15:15:31 +0200

    added model for comments

---
+++ task-1/app/models/comments.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, EmailStr
+from typing import Optional
+from bson import ObjectId
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_pydantic_core_schema__(cls, *args, **kwargs):
+        from pydantic_core import core_schema
+        return core_schema.str_schema()
+
+    @classmethod
+    def __get_pydantic_json_schema__(cls, schema, handler):
+        return {"type": "string"}
+
+
+class CommentModel(BaseModel):
+    id: int = Field(..., description="JSONPlaceholder comment ID")
+    postId: int = Field(..., description="ID of the related post")
+    name: str
+    email: EmailStr
+    body: str
