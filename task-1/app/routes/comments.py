a058bf751e4ab4c569d05dafe6a40ba5bfbf20bb
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-27 15:15:04 +0200

    added routes for comments resource

---
+++ task-1/app/routes/comments.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+from ..database import mongo_connector
+from ..models.comments import CommentModel
+
+route = APIRouter()
+
+@route.get("/comments", response_model=List[CommentModel])
+async def get_comments():
+    try:
+        # Attempt to fetch comments from the database
+        comments = await mongo_connector.mongodb.db['comments'].find().to_list(length=None)
+        return comments
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
