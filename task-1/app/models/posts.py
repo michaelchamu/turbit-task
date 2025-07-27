e21e70955f48a10423ceff430900c1eb3fd2571c
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-27 14:57:40 +0200

    added posts model

---
+++ task-1/app/models/posts.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_pydantic_core_schema__(cls, *args, **kwargs):
+        from pydantic_core import core_schema
+        return core_schema.str_schema()
+
+    @classmethod
+    def __get_pydantic_json_schema__(cls, schema, handler):
+        return {"type": "string"}
+
+
+class PostModel(BaseModel):
+    id: int = Field(..., description="JSONPlaceholder post ID")
+    userId: int = Field(..., description="ID of the user who created the post")
+    title: str
+    body: str
