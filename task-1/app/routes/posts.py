99adba0d9924dee9d02e4757ae01b9725348a9c2
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-27 14:57:15 +0200

    added get routes for the posts endpoint

---
+++ task-1/app/routes/posts.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+from ..database import mongo_connector
+from ..models.posts import PostModel
+
+route = APIRouter()
+
+@route.get("/posts", response_model=List[PostModel])
+async def get_posts():
+    try:
+        # Attempt to fetch posts from the database
+        posts = await mongo_connector.mongodb.db['posts'].find().to_list(length=None)
+        return posts
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
