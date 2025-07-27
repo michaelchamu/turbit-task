f597ebcf44fca0ebd1f66d9a2dd339da9ad0182b
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-27 16:33:28 +0200

    added call to database populate routine

--- task-1/app/main.py
+++ task-1/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI #get the FastAPI and other modules from it
 from .database import mongo_connector #import the MongoDB connection functions
+from .services import populate_db
 from .routes import users, posts, comments, reports #import all defined routes
 
 app = FastAPI() #initialize the FastAPI application
@@ -11,9 +12,14 @@ app = FastAPI() #initialize the FastAPI application
 #The lifespan context manager allows for setup and teardown operations around the application lifecycle
 #This is useful for managing resources like database connections, ensuring they are opened when the app starts
 #and closed when the app stops, preventing resource leaks and ensuring clean shutdowns.
+#when application starts, and connects to MongoDB, Check if the 3 required collections exist.
+#if they dont exist, create them and populate them with data from the JSON Placeholder API.
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await mongo_connector.connect_to_mongo()
+    await populate_db.populate_db(mongo_connector.mongodb.db)
+    print("Application started and connected to MongoDB")
     yield   # This is where the application runs
     await mongo_connector.close_mongo_connection()
 
