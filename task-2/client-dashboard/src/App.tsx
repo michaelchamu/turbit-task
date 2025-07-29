375695d228386cfb7d2efaf7ffa6cfa356096f20
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-29 19:08:51 +0200

    added filters and display options for graph

--- task-2/client-dashboard/src/App.tsx
+++ task-2/client-dashboard/src/App.tsx
@@ -2,18 +2,28 @@ import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Sidebar from './components/charts/SidebarControl';
 import TimeseriesChart from './components/charts/TimeSeriesChart';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [filters, setFilters] = useState<{
+    turbineId?: string;
+    startDate?: string;
+    endDate?: string;
+  }>({});
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow container mx-auto px-4 py-6">
         <div className='grid grid-cols-1 lg:grid-cols-12 gab-6'>
           <div className='lg:col-span-3'>
-            <Sidebar />
+            <Sidebar onFilterChange={setFilters} />
           </div>
           <div className='lg:col-span-9'>
-            <TimeseriesChart />
+            <TimeseriesChart
+              turbineId={filters.turbineId}
+              startDate={filters.startDate}
+              endDate={filters.endDate}
+            />
           </div>
         </div>
       </main>
