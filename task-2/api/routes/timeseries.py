bc74f5aab01c4bd89b7356bb319c2e373ff74d16
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-29 17:56:22 +0200

    Adjusted endpoint to allow filtering based on date etc

--- task-2/api/routes/timeseries.py
+++ task-2/api/routes/timeseries.py
@@ -1,19 +1,45 @@
-from fastapi import APIRouter, HTTPException
-from typing import List
+from fastapi import APIRouter, HTTPException, Query
+from typing import List, Optional
+from datetime import datetime
+from pymongo import ASCENDING
+
 from ..database import mongo_connector
 from ..models.timeseries import TimeSeriesModel
 
 route = APIRouter()
 
 @route.get("/timeseries", response_model=List[TimeSeriesModel])
-async def get_time_series_data():
+async def get_time_series_data(
+    turbine_id: Optional[str] = None,
+    start_date: Optional[datetime] = None,
+    end_date: Optional[datetime] = None,
+    limit: int = Query(1000, gt=0, le=10000),
+):
+    # Fetch time series data from the database
     try:
-        # Fetch time series data from the database
-        time_series_data = await mongo_connector.mongodb.db['time-series-data'].find().to_list(length=100)
-        if not time_series_data:
-            raise HTTPException(status_code=404, detail="No time series data found")
-        
+        query = {}
+        if turbine_id:
+            query['turbine_id'] = turbine_id
+
+        if start_date and end_date:
+            query['timestamp'] = {
+                '$gte': start_date,
+                '$lte': end_date
+            }
+        elif start_date:
+            query['timestamp'] = {'$gte': start_date}
+        elif end_date:
+            query['timestamp'] = {'$lte': end_date}
+
+        cursor = mongo_connector.mongodb.db['time-series-data'].find(query).sort('timestamp', ASCENDING).limit(limit)
+        results = await cursor.to_list(length=limit)
+
         # Convert the fetched data to TimeSeriesModel instances
-        return [TimeSeriesModel(**data) for data in time_series_data]
+        return [TimeSeriesModel(**data) for data in results]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+     
+
+        
+
+        
