f48c4c28834ac5fa9813a8fd30eab3208b89b999
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-29 18:20:34 +0200

    adjusted service and added interfaces for data model and used axios for data fetching with promises included

---
+++ task-2/client-dashboard/src/services/apiService.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_ENDPOINT = import.meta.env.API_ENDPOINT || 'http://localhost:8000';
+
+export interface TimeSeriesData {
+    timestamp: string;
+    power: number;
+    wind_speed: number;
+    turbine_id: string;
+    additional_data?: Record<string, any>;
+}
+
+interface QueryParameters {
+    turbine_id?: string;
+    start_date?: string;
+    end_date?: string;
+    limit?: number;
+}
+
+
+export const fetchTimeSeriesData = async (
+    params?: QueryParameters
+): Promise<TimeSeriesData[]> => {
+    try {
+        const response = await axios.get<TimeSeriesData[]>(`${API_ENDPOINT}/timeseries`, {
+            params,
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching time series data:', error);
+        throw error; // rethrow the error for further handling
+    }
+};
