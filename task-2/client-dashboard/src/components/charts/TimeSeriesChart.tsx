a34e217fa91f4bee843ab5e432d55f6f4409e229
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-31 21:40:41 +0200

    adjust mapping and filtering for max windspeed and power generated to eliminate the occurence of infinity readings whenever there is no data in the aggregation

--- task-2/client-dashboard/src/components/charts/TimeSeriesChart.tsx
+++ task-2/client-dashboard/src/components/charts/TimeSeriesChart.tsx
@@ -53,8 +53,18 @@ const TimeSeriesChart = ({ turbineId, startDate, endDate }: TimeSeriesChartProps
   }, [turbineId, startDate, endDate]);
 
   // get statistical summaries
-  const maxWindSpeed = Math.max(...data.map(d => d.average_wind_speed));
-  const maxPower = Math.max(...data.map(d => d.average_power));
+  //step is important to remove any NaN or other wild values
+  //before adding this step, -Infinity was being returned whenever theres no data from the API
+  const validWindSpeeds = data
+    .map(d => d.average_wind_speed)
+    .filter(p => Number.isFinite(p));
+  const maxWindSpeed = validWindSpeeds.length > 0 ? Math.max(...validWindSpeeds) : 0;
+
+  const validPowers = data
+    .map(d => d.average_power)
+    .filter(p => Number.isFinite(p));
+  const maxPower = validPowers.length > 0 ? Math.max(...validPowers) : 0;
+
   const avgPower = data.reduce((acc, item) => acc + item.average_power, 0) / data.length;
   const avgWindSpeed = data.reduce((acc, item) => acc + item.average_wind_speed, 0) / data.length;
   const locationNames = ['Bremen', 'Oldenburg', 'Hamburg', 'Bochum', 'Borkum', 'Stuttgart'] // pick a random location name where the turbine is located
@@ -122,7 +132,7 @@ const TimeSeriesChart = ({ turbineId, startDate, endDate }: TimeSeriesChartProps
           <div>
             <div className="text-sm text-gray-600">Avg Power</div>
             <div className="text-lg font-semibold text-gray-800">
-              {avgPower.toFixed(0)} kW
+              {isNaN(avgPower) ? 0.0 : avgPower.toFixed(0)} kW
             </div>
           </div>
         </div>
@@ -133,7 +143,7 @@ const TimeSeriesChart = ({ turbineId, startDate, endDate }: TimeSeriesChartProps
           </div>
           <div className="text-sm text-black-600">Max Power</div>
           <div className="text-lg font-semibold text-gray-800">
-            {maxPower.toFixed(0)} kW
+            {isNaN(maxPower) ? 0.0 : maxPower.toFixed(0)} kW
           </div>
         </div>
 
@@ -142,14 +152,18 @@ const TimeSeriesChart = ({ turbineId, startDate, endDate }: TimeSeriesChartProps
             <FaWind className="w-5 h-5" />
           </div>
           <div className="text-sm text-gray-600">Avg Wind Speed</div>
-          <div className="text-lg font-semibold">{avgWindSpeed.toFixed(1)} m/s</div>
+          <div className="text-lg font-semibold">
+            {isNaN(avgWindSpeed) ? 0.0 : avgWindSpeed.toFixed(1)} m/s
+          </div>
         </div>
         <div className="bg-blue-300 p-3 rounded-lg shadow-sm flex items-center gap-3">
           <div className="bg-blue-300 text-blue-600 rounded-full p-2">
             <IoMdSpeedometer className="w-5 h-5" />
           </div>
           <div className="text-sm text-gray-600">Max Wind Speed</div>
-          <div className="text-lg font-semibold">{maxWindSpeed.toFixed(0)} m/s</div>
+          <div className="text-lg font-semibold">
+            {isNaN(maxWindSpeed) ? 0.0 : maxWindSpeed.toFixed(0)} m/s
+          </div>
         </div>
       </div>
       <br />
@@ -159,28 +173,36 @@ const TimeSeriesChart = ({ turbineId, startDate, endDate }: TimeSeriesChartProps
             <SiProtractor className="w-5 h-5" />
           </div>
           <div className="text-sm text-gray-600">Avg Azimuth</div>
-          <div className="text-lg font-semibold">{avgAzimuth.toFixed(0)} &deg;</div>
+          <div className="text-lg font-semibold">
+            {isNaN(avgAzimuth) ? 0.0 : avgAzimuth.toFixed(0)} &deg;
+          </div>
         </div>
         <div className="bg-red-200 p-3 rounded-lg shadow-sm flex items-center gap-3">
           <div className="bg-red-200 text-red-600 rounded-full p-2">
             < LiaTemperatureHighSolid className="w-5 h-5" />
           </div>
           <div className="text-sm text-gray-600">Avg External Temp</div>
-          <div className="text-lg font-semibold">{avgExternalTemperature.toFixed(1)} &deg;C</div>
+          <div className="text-lg font-semibold">
+            {isNaN(avgExternalTemperature) ? 0.0 : avgExternalTemperature.toFixed(1)} &deg;C
+          </div>
         </div>
         <div className="bg-cyan-200 p-3 rounded-lg shadow-sm flex items-center gap-3">
           <div className="bg-cyan-200 text-cyan-600 rounded-full p-2">
             < LiaTemperatureHighSolid className="w-5 h-5" />
           </div>
           <div className="text-sm text-gray-600">Avg Internal Temp</div>
-          <div className="text-lg font-semibold">{avgInternalTemperature.toFixed(1)} &deg;C</div>
+          <div className="text-lg font-semibold">
+            {isNaN(avgInternalTemperature) ? 0.0 : avgInternalTemperature.toFixed(1)} &deg;C
+          </div>
         </div>
         <div className="bg-purple-200 p-3 rounded-lg shadow-sm flex items-center gap-3">
           <div className="bg-purple-200 text-purple-600 rounded-full p-2">
             <MdOutlineWindPower className="w-5 h-5" />
           </div>
           <div className="text-sm text-gray-600">Avg Rotor Speed</div>
-          <div className="text-lg font-semibold">{avgRPM.toFixed(1)} rpm</div>
+          <div className="text-lg font-semibold">
+            {isNaN(avgRPM) ? 0.0 : avgRPM.toFixed(1)} rpm
+          </div>
         </div>
       </div>
 
