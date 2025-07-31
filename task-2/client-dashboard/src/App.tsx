088a2237ec93c741e96a43adfd997965f5d5d370
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-07-31 14:20:27 +0200

    removed reference to sidebar control evaluation copy

--- task-2/client-dashboard/src/App.tsx
+++ task-2/client-dashboard/src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import TimeseriesChart from './components/charts/TimeSeriesChart';
 import { useState } from 'react';
-import SidebarControl from './components/charts/sidebar';
+import SidebarControl from './components/charts/SidebarControl';
 
 const App = () => {
   const [filters, setFilters] = useState<{
@@ -16,11 +16,9 @@ const App = () => {
       <main className="flex-grow container mx-auto px-4 py-6">
         <div className='grid grid-cols-1 lg:grid-cols-12 gab-6'>
           <div className='lg:col-span-3'>
-            {/* <Sidebar onFilterChange={setFilters} /> */}
             <SidebarControl onFilterChange={setFilters} />
           </div>
           <div className='lg:col-span-9'>
-
             <TimeseriesChart
               turbineId={filters.turbineId}
               startDate={filters.startDate}
