fd8c1dc3a5e39993127637152929088b39508b49
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-03 14:20:36 +0200

    adjusted return values

--- task-2/client-dashboard/src/services/apiService.ts
+++ task-2/client-dashboard/src/services/apiService.ts
@@ -1,6 +1,6 @@
 import type { TimeSeriesDataPoint, QueryParameters } from '../types/interfaces';
 import { _get } from '../utils/http_utility';
-
+import logger from '../utils/logger';
 
 export const fetchTimeSeriesData = async (
     params?: QueryParameters
@@ -9,14 +9,13 @@ export const fetchTimeSeriesData = async (
         const response = await _get(`/aggregated_timeseries`, {
             params
         });
-        if (response.status === 200 || response.status === 204) {
+        if (response.status === 200)
             return response.data;
-        } else {
-            throw new Error(`Unexpected response status: ${response.status}`);
-        }
-
+        if (response.status === 204)
+            return [];
+        throw new Error(`Unexpected response status: ${response.status}`);
     } catch (error) {
-        console.error('Error fetching time series data:', error);
+        logger.error('Error fetching time series data:', error);
         throw error; // rethrow the error for further handling
     }
 };
@@ -27,12 +26,12 @@ export const fetchTurbineList = async ():
         const response = await _get(`/turbines`);
         //check if there are turbines in list or not
         if (response.status === 204) {
-            console.info('No Turbines');
+            logger.info('No Turbines');
             return [];// just set the dropdown to empty for now
         }
         return response.data;
     } catch (error) {
-        console.error('Error fetching Turbines List:', error);
+        logger.error('Error fetching Turbines List:', error);
         throw error;
     }
 }
