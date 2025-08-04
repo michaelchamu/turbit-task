e19222e35ab8e0fb35ec4f90dffb575d5ab8045c
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-04 18:16:55 +0200

    added fetch single user

--- task-1/app/routes/users.py
+++ task-1/app/routes/users.py
@@ -21,3 +21,14 @@ async def get_users():
         return users
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+@route.get("/users/{user_id}", response_model=UserModel)
+async def get_single_user(user_id: int):
+    try:
+        user = await mongo_connector.mongodb.db['users'].find_one({"id": user_id})
+        if not user:
+            raise HTTPException(status_code=404, detail="Post not found")
+        return user
+    except Exception as e:
+         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Unexpected error: " + str(e)) 
+
