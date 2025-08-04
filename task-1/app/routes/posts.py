01f32d85f712bb30994b728ae344b0c94beafc2a
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-04 19:13:52 +0200

    http status codes fix

--- task-1/app/routes/posts.py
+++ task-1/app/routes/posts.py
@@ -7,7 +7,20 @@ from ..models.posts import PostModel
 
 route = APIRouter()
 
-@route.get("/posts", response_model=List[PostModel])
+@route.get("/posts",
+           response_model=List[PostModel],
+           description="Fetches full list of posts in collection",
+           responses={
+               status.HTTP_200_OK:{
+                   "description": "All posts were fetched successfully"
+               },
+               status.HTTP_204_NO_CONTENT:{
+                   "description": "Query ran successfully but there are no posts to show"
+               },
+               status.HTTP_500_INTERNAL_SERVER_ERROR:{
+                   "description": "There was an error in the code, check exception body for details"
+               }
+               })
 async def get_posts():
     try:
         # Attempt to fetch posts from the database
@@ -23,7 +36,20 @@ async def get_posts():
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     
-@route.get("/posts/{post_id}", response_model=PostModel)
+@route.get("/posts/{post_id}",
+           response_model=PostModel,
+           description="Fetches single post by ID in collection",
+           responses={
+               status.HTTP_200_OK:{
+                   "description": "Single post retrieval successful"
+               },
+               status.HTTP_404_NOT_FOUND:{
+                   "description": "Single post with id not found"
+               },
+               status.HTTP_500_INTERNAL_SERVER_ERROR:{
+                   "description": "There was an error in the code, check exception body for details"
+               }
+               })
 async def get_single_post(post_id: int):
     try:
         post = await mongo_connector.mongodb.db['posts'].find_one({"id": post_id})
