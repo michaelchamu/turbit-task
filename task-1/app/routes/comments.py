bb2be3a26c4aa52eb39700ddde62fce100f6a8a3
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-04 08:48:43 +0200

    check for empty data return

--- task-1/app/routes/comments.py
+++ task-1/app/routes/comments.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from typing import List
+
+from fastapi.responses import JSONResponse
 from mongoconnector import mongo_connector
 from ..models.comments import CommentModel
 
@@ -10,6 +12,11 @@ async def get_comments():
     try:
         # Attempt to fetch comments from the database
         comments = await mongo_connector.mongodb.db['comments'].find().to_list(length=None)
+        if not comments:
+            return JSONResponse(
+                status_code=204,
+                content=[]
+            )
         return comments
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
