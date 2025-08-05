24bc5c1971b46cc749711e3e3bbe58d0acba1f0b
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-05 12:41:22 +0200

    added custom logging

--- task-1/app/routes/posts.py
+++ task-1/app/routes/posts.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, HTTPException, Response, status
 from typing import List
-
+import logging
 from fastapi.responses import JSONResponse
 from mongoconnector import mongo_connector
 from ..models.posts import PostModel
 
 route = APIRouter()
+logger = logging.getLogger("task-1")
 
 @route.get("/posts", response_model=List[PostModel])
 async def get_posts():
@@ -24,4 +25,5 @@ async def get_single_post(post_id: int):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
         return post
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Unexpected error: " + str(e)) 
+        logger.error(str(e))
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Unexpected error") 
