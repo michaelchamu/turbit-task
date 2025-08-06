95de3691bbd1747695416663e2178eec9a26022b
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-06 16:15:21 +0200

    moved header and footer to layout file

--- task-2/client-dashboard/src/App.tsx
+++ task-2/client-dashboard/src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import { ToastContainer } from 'react-toastify';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
 import TimeseriesChart from './components/charts/TimeSeriesChart';
 import SidebarControl from './components/controls/SidebarControl';
 import Overlay from './components/common/Overlay';
+import Layout from './components/layout/Layout';
 
 const App = () => {
   const [filters, setFilters] = useState<{
@@ -16,6 +15,7 @@ const App = () => {
   const [showOverlay, setShowOverlay] = useState<boolean>(true);
 
   useEffect(() => {
+
     const timer = setTimeout(() => {
       setShowOverlay(false);
     }, 3000);
@@ -26,25 +26,23 @@ const App = () => {
   return (
     <>
       {showOverlay && <Overlay />}
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-6">
-          <ToastContainer position="top-center" autoClose={3000} />
-          <div className='grid grid-cols-1 lg:grid-cols-12 gap-6'>
-            <div className='lg:col-span-3'>
-              <SidebarControl onFilterChange={setFilters} />
-            </div>
-            <div className='lg:col-span-9'>
-              <TimeseriesChart
-                turbineId={filters.turbineId}
-                startDate={filters.startDate}
-                endDate={filters.endDate}
-              />
-            </div>
+      <Layout >
+
+        <ToastContainer position="top-center" autoClose={3000} />
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-6'>
+          <div className='lg:col-span-3'>
+            <SidebarControl onFilterChange={setFilters} />
           </div>
-        </main>
-        <Footer />
-      </div>
+          <div className='lg:col-span-9'>
+            <TimeseriesChart
+              turbineId={filters.turbineId}
+              startDate={filters.startDate}
+              endDate={filters.endDate}
+            />
+          </div>
+        </div>
+
+      </Layout>
     </>
   );
 }
