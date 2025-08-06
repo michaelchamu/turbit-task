d07fc3cb49b6af591f9522b2a3ab2039531a16d9
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-06 22:39:49 +0200

    adjusted the init-script for creating indexes on build

--- db/init-mongo.js
+++ db/init-mongo.js
@@ -1,7 +1,22 @@
-// Create users data database for 1st task with no collection
-db = db.getSiblingDB('users-data')
+//create user_data table and indexes for collations
+db = db.getSiblingDB('users_data');
 
-// Create time series database for 2nd task with no collection
-db = db.getSiblingDB('time-series-data')
+// Create text indexes for partial text searches with case-insensitive collation
+db.createCollection('users');
+db.users.createIndex({ "username": "text" });
 
-print("Initialized databases: users_data and times_series_data")
+db.createCollection('posts');
+db.posts.createIndex({ "userId": 1 }, { unique: true });
+db.createCollection('comments');
+db.comments.createIndex({ "postId": 1 }, { unique: true });
+
+//create time_series_data following the timeseries schema
+db = db.getSiblingDB('time_series_data');
+
+db.createCollection("turbine_readings", {
+    timeseries: {
+        timeField: "timestamp",
+        metaField: "metadata",
+        granularity: "minutes"
+    }
+});
