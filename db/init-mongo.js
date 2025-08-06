5dd187a8b407ff0ccfbe1a156e07df7613193e4f
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-06 23:08:06 +0200

    removed indexes for posts and comments because there are dublicate values

--- db/init-mongo.js
+++ db/init-mongo.js
@@ -3,12 +3,12 @@ db = db.getSiblingDB('users_data');
 
 // Create text indexes for partial text searches with case-insensitive collation
 db.createCollection('users');
+//this will make it easier to search partially by username
 db.users.createIndex({ "username": "text" });
 
 db.createCollection('posts');
-db.posts.createIndex({ "userId": 1 }, { unique: true });
+//these are set to false because there are identical values in some documents from the api
 db.createCollection('comments');
-db.comments.createIndex({ "postId": 1 }, { unique: true });
 
 //create time_series_data following the timeseries schema
 db = db.getSiblingDB('time_series_data');
