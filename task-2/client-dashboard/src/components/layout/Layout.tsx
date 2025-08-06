602abff2bebbdcf6129a7d73cf50021812157bb1
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-06 16:14:40 +0200

    moved header and footer to reusable layout component

---
+++ task-2/client-dashboard/src/components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import Header from '../common/Header';
+import Footer from '../common/Footer';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow container mx-auto px-4 py-6">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
+}
