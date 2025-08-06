6616db2b6b55c23424180bb335e48ef5a17631d8
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-06 16:15:54 +0200

    dummy settings page

---
+++ task-2/client-dashboard/src/components/common/Settings.tsx
@@ -0,0 +1,14 @@
+import Layout from "../layout/Layout";
+
+
+export default function Settings() {
+    return (
+        <Layout>
+            <div className="flex-grow container mx-auto px-4 py-6">
+                <h2 className="text-2xl font-bold mb-4">Settings</h2>
+                <p>This is the settings page.</p>
+            </div>
+
+        </Layout>
+    );
+}
