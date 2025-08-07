a24488c5cf65d80fc0a03357789f65f08aea6a92
Michael Chamunorwa <chamunorwamichael@gmail.com>
2025-08-07 16:38:25 +0200

    default configs for client demo

---
+++ task-1/client-demo/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import UserReportsList from './components/UserReportList'
+
+
+function App() {
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold mb-6">User Reports</h1>
+      <UserReportsList />
+    </div>
+  )
+}
+
+export default App
